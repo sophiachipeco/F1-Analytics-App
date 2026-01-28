d00b6aeb77fc99821d78758e968a82c15f08ebaa
Emmy <emmyfong@gmail.com>
2026-01-28 09:47:25 -0800

    Updated requirements.txt and main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,4 +29,4 @@ def get_session(year: int, location: str, type: str):
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
